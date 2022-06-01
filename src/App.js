66010e7ce8b11ee806e64d4dc65a43f75859bfa2
Karol <kariurek@gmail.com>
2022-06-01 15:09:04 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import './assets/css/index.css';
+
+const App = (props) => {
+	return <div>Hello world z rana</div>;
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import App from './App';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('app');
+ReactDOM.render(<App />, app);
