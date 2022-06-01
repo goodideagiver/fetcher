04cf744f4a61a8b8fe30d6e65662642b32aa03dd
prplblck <kariurek@gmail.com>
2022-06-01 18:53:48 +0200

    Add universal buttton component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './assets/css/index.css';
+import './assets/css/variables.css';
 
 import Button from './components/ui/Button/Button';
 
 const App = () => {
 	return (
 		<div>
-			<button>Siema z buttona</button>
 			<Button primary>Important button</Button>
 			<Button>Normal button</Button>
 			<Button secondary>Not important button</Button>
--- src/components/ui/Button/Button.js
+++ src/components/ui/Button/Button.js
@@ -1,11 +1,40 @@
 import * as classes from './Button.module.css';
 
-const Button = ({ className, children, ...props }) => {
+import { Link } from 'react-router-dom';
+
+const Button = ({ className, children, to, href, ...props }) => {
+	let buttonVariant = classes.button;
+
+	if (props.primary) {
+		buttonVariant = classes.buttonPrimary;
+	}
+
+	if (props.secondary) {
+		buttonVariant = classes.buttonSecondary;
+	}
+
+	const buttonClasses = `${classes.base} ${buttonVariant} ${
+		className ? className : ''
+	} `;
+
+	if (to) {
+		return (
+			<Link to={to} className={buttonClasses} {...props}>
+				{children}
+			</Link>
+		);
+	}
+
+	if (href) {
+		return (
+			<a href={href} className={buttonClasses} {...props}>
+				{children}
+			</a>
+		);
+	}
+
 	return (
-		<button
-			className={classes.button + ' ' + (className ? className : '')}
-			{...props}
-		>
+		<button className={buttonClasses} {...props}>
 			{children}
 		</button>
 	);
