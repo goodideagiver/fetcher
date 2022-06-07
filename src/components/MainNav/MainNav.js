615ad48f0b25be3f1d0b07cb3453a715720a0c26
Karol <kariurek@gmail.com>
2022-06-07 08:21:09 +0200

    Refactor main nav content

--- src/components/MainNav/MainNav.js
+++ src/components/MainNav/MainNav.js
@@ -4,18 +4,19 @@ import useMediaQuery from '../../hooks/useMediaQuery';
 const MainNav = () => {
 	const isMobile = useMediaQuery('(max-width: 768px)');
 
+	const headerContent = !isMobile ? (
+		<>
+			Fetcher<span>.js</span>
+		</>
+	) : (
+		<>
+			F<span>.js</span>
+		</>
+	);
+
 	return (
 		<header className={styles.header}>
-			{!isMobile && (
-				<h1 className={styles.title}>
-					Fetcher<span>.js</span>
-				</h1>
-			)}
-			{isMobile && (
-				<h1 className={styles.title}>
-					F<span>.js</span>
-				</h1>
-			)}
+			<h1 className={styles.title}>{headerContent}-</h1>
 		</header>
 	);
 };
