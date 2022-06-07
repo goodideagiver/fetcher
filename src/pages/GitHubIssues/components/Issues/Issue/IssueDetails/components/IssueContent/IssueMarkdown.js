d88e2ef7837974947d6c332bc4146ec019643392
Karol <kariurek@gmail.com>
2022-06-07 08:29:42 +0200

    Refactor issue markdown component

--- src/pages/GitHubIssues/components/Issues/Issue/IssueDetails/components/IssueContent/IssueMarkdown.js
+++ src/pages/GitHubIssues/components/Issues/Issue/IssueDetails/components/IssueContent/IssueMarkdown.js
@@ -1,20 +1,20 @@
 import ReactMarkdown from 'react-markdown';
 import classes from './IssueContent.module.css';
 
+const reactMarkdownComponentConfig = {
+	img({ src }) {
+		return (
+			<a href={src} target='_blank' rel='noreferrer noopener'>
+				<img src={src} />
+			</a>
+		);
+	},
+};
+
 const IssueMarkdown = ({ description }) => {
 	return (
 		<div className={classes.md}>
-			<ReactMarkdown
-				components={{
-					img({ src }) {
-						return (
-							<a href={src} target='_blank' rel='noreferrer noopener'>
-								<img src={src} />
-							</a>
-						);
-					},
-				}}
-			>
+			<ReactMarkdown components={reactMarkdownComponentConfig}>
 				{description}
 			</ReactMarkdown>
 		</div>
