6ab3c65d005b64a881af9100cf0023c689c227f5
prplblck <kariurek@gmail.com>
2022-06-09 22:24:14 +0200

    Add sidebar transition animation

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,22 +4,54 @@ import ReactDOM from 'react-dom';
 import styles from './Sidebar.module.css';
 import { useState } from 'react';
 import Links from './Links/Links';
+import { CSSTransition } from 'react-transition-group';
+
+const AnimatedSidebar = ({ visible }) => {
+	return (
+		<CSSTransition
+			mountOnEnter
+			unmountOnExit
+			in={visible}
+			timeout={500}
+			classNames={{
+				enter: styles.sidebarEnter,
+				exit: styles.sidebarExit,
+			}}
+		>
+			<div className={styles.sidebar}>
+				<Links />
+			</div>
+		</CSSTransition>
+	);
+};
+
+const AnimatedSidebarToggleButton = ({ toggleSidebar, visible }) => (
+	<CSSTransition
+		in={visible}
+		timeout={500}
+		classNames={{
+			enterActive: styles.buttonEnter,
+			exit: styles.buttonExit,
+		}}
+	>
+		<button onClick={toggleSidebar} className={styles.button}>
+			{visible ? <AiOutlineLeft /> : <AiOutlineRight />}
+		</button>
+	</CSSTransition>
+);
 
 const Sidebar = () => {
 	const [isOpen, setIsOpen] = useState(false);
 
-	const toggleSidebar = () => setIsOpen(!isOpen);
+	const toggleSidebarHandler = () => setIsOpen(!isOpen);
 
 	return ReactDOM.createPortal(
 		<div className={styles.wrapper}>
-			{isOpen && (
-				<div className={styles.sidebar}>
-					<Links/>
-				</div>
-			)}
-			<button onClick={toggleSidebar} className={styles.button}>
-				{isOpen ? <AiOutlineLeft /> : <AiOutlineRight />}
-			</button>
+			<AnimatedSidebar visible={isOpen} />
+			<AnimatedSidebarToggleButton
+				visible={isOpen}
+				toggleSidebar={toggleSidebarHandler}
+			></AnimatedSidebarToggleButton>
 		</div>,
 		document.getElementById('overlay-root')
 	);
